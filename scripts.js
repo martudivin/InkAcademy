d24e17d8b33d1731e70c711c11c4dbafeee096e6
martudivin <martudivin@gmail.com>
2023-11-10 21:53:21 -0300

    commiteando

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
-const navbar = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar"); 
+// const navbar = document.querySelector("nav");
+// const abrir = document.querySelector("abrir");
+// const cerrar = document.querySelector("cerrar"); 
 
-navbar.addEventListener("click", () => {
-    abrir.classList.add("visible");
-})
+// navbar.addEventListener("click", () => {
+//     abrir.classList.add("visible");
+// });
 
+// navbar.addEventListener("click", () => {
+//     cerrar.classList.remove("visible");
+// });
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    // console.log("DOM cargado")
+
+    const navbar = document.querySelector("nav");
+    const abrir = document.querySelector("abrir");
+    const cerrar = document.querySelector("cerrar");
+
+    navbar.addEventListener("click", () => {
+        abrir.classList.toogle("visible");
+    });
+    
+});
+
+// navbar.addEventListener("click", () => {
+//     cerrar.classList.remove("visible");
+// });
 
 
